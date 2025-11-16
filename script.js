9f44aaf9415f0b00c166058f91d75a3d5df31787
JonGudenzi <jongudenzi@gmail.com>
2025-11-16 11:08:58 -0500

    delete task logic

--- script.js
+++ script.js
@@ -17,18 +17,20 @@ function addTask(text) {
 
 function render() {
     taskList.innerHTML = "";
-    tasks.forEach(function(task, index) {
+    tasks.forEach(function (task, index) {
         const li = document.createElement("li");
         li.textContent = task.text;
         li.dataset.index = index;
-        if (task.completed === true) {li.classList.add("completed");};
+        if (task.completed === true) { li.classList.add("completed"); };
         li.addEventListener("click", toggleTask);
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "X";
         li.appendChild(deleteBtn);
-        deleteBtn.addEventListener("click", function(event) {
+        deleteBtn.addEventListener("click", function (event) {
             event.stopPropagation();
-
+            const li = event.target.closest("li");
+            const index = li.dataset.index;
+            deleteTask(index);
         })
         taskList.appendChild(li);
     })
@@ -36,7 +38,7 @@ function render() {
 
 function toggleTask(event) {
     const li = event.target;
-   const index = li.dataset.index;
+    const index = li.dataset.index;
     tasks[index].completed = !tasks[index].completed;
     render();
     saveTask();
@@ -44,30 +46,34 @@ function toggleTask(event) {
 
 function deleteTask(index) {
     tasks.splice(index, 1);
+    taskInput.value = "";
+    taskInput.focus();
     render();
     saveTask();
 }
 
 function saveTask() {
-    
+
 }
 
 function loadTask() {
-    
+
 }
 
 taskForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-event.preventDefault();
-const userInput = taskInput.value.trim();
-if (userInput === "") {
-    errorMsg.textContent = "Please enter a task";
+    event.preventDefault();
+    const userInput = taskInput.value.trim();
+    if (userInput === "") {
+        errorMsg.textContent = "Please enter a task";
+        taskInput.focus();
+        return;
+    }
+    errorMsg.textContent = "";
+    taskInput.value = "";
     taskInput.focus();
-    return;
-}
-errorMsg.textContent = "";
-addTask(userInput);
-render();
+    addTask(userInput);
+    render();
 }
 loadTask();
